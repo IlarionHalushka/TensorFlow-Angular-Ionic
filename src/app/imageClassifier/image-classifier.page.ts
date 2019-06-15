e70a2a987a1f966c0318c44b83fe32ceab944c38
IlarionHalushka <hilarion.galushka@gmail.com>
2019-06-15 23:35:42 +0300

    add image picker to image classifier example; predict on image change

--- src/app/imageClassifier/image-classifier.page.ts
+++ src/app/imageClassifier/image-classifier.page.ts
@@ -1,10 +1,11 @@
-import {Component, OnInit} from '@angular/core';
-import * as tf from '@tensorflow/tfjs';
+import {Component, Input, OnInit} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
-import {ResponseContentType} from '@angular/http';
+import { FileUploader } from 'ng2-file-upload';
 
 declare let ml5: any;
 declare let require: any;
+
+const URL = '.';
 
 @Component({
   selector: 'app-image-classifier',
@@ -12,44 +13,98 @@ declare let require: any;
   styleUrls: ['image-classifier.page.scss']
 })
 export class ImageClassifierPage implements OnInit {
-  classifier: any;
   result: any;
-  image: any;
+  imageSrc: string | ArrayBuffer;
+  src = '../../assets/images/plane.jpg';
   probability: any;
-  constructor(private http: HttpClient) {}
+  results = [{
+    className : null,
+    probability: null,
+  }];
+  inferenceTime = null;
+  IMAGE_SIZE = 224;
+  predictions: Array<object>;
+
+  @Input() classifier;
+  @Input() image;
+
+  // public uploader: FileUploader = new FileUploader({ url: URL });
+
+  constructor(private http: HttpClient) {
+    // this.setImageSrc();
+  }
+
+  // async setImageSrc() {
+  //   const reader = new FileReader();
+  //
+  //   reader.onload = (event) => { // called once readAsDataURL is completed
+  //     this.src = event.target.result;
+  //   };
+  //
+  //   reader.readAsDataURL(this.image.rawFile); // read file as data url
+  // }
 
   ngOnInit() {
     this.classifier = ml5.imageClassifier('MobileNet', () => {
       console.log('Model Loaded!');
-      this.predict();
+    });
+    // this.setImageSrc();
+  }
+
+  // async classifyImage(image) {
+  //   image.width = this.IMAGE_SIZE;
+  //   image.height = this.IMAGE_SIZE;
+  //
+  //   const startTime = performance.now();
+  //   this.results = await this.classifier.classifyImage(image);
+  //   this.inferenceTime = Math.floor(performance.now() - startTime);
+  //   console.log(this.results);
+  // }
+
+  // createImageFromBlob(image: Blob) {
+  //   const reader = new FileReader();
+  //   reader.addEventListener('load', () => {
+  //     this.image = reader.result;
+  //   }, false);
+  //
+  //   if (image) {
+  //     reader.readAsDataURL(image);
+  //   }
+  // }
+
+  predict(img) {
+    // console.log('image:', this.image);
+    // // this.createImageFromBlob(this.image);
+    // console.log('image:', this.image);
+    // const reader = new FileReader();
+    // reader.readAsDataURL(image.rawFile);
+    console.log(img);
+    console.log(img instanceof HTMLImageElement);
+    console.log(img instanceof HTMLCanvasElement);
+    // this.classifier.classify(img, (err, results) => {
+    //   // this.result = results[0].className;
+    //   // this.probability = results[0].probability.toFixed(4);
+    //   console.log(results);
+    // });
+    this.classifier.predict(img, (err, results) => {
+      // this.result = results[0].className;
+      // this.probability = results[0].probability.toFixed(4);
+      this.predictions = results;
+      console.log(results);
     });
   }
 
-  createImageFromBlob(image: Blob) {
-    const reader = new FileReader();
-    reader.addEventListener('load', () => {
-      this.image = reader.result;
-    }, false);
+  readURL(event: Event): void {
+    if (event.target.files && event.target.files[0]) {
+      const file = event.target.files[0];
 
-    if (image) {
-      reader.readAsDataURL(image);
+      file.width = this.IMAGE_SIZE;
+      file.height = this.IMAGE_SIZE;
+
+      const reader = new FileReader();
+      reader.onload = e => this.imageSrc = reader.result;
+
+      reader.readAsDataURL(file);
     }
-  }
-
-  predict() {
-    this.http.get(
-        'https://images.unsplash.com/photo-1556228453-46a07da3df9a?ixlib=rb-1.2.1&q=85&fm=jpg&crop=entropy&cs=srgb&dl=curology-1540938-unsplash.jpg',
-        { responseType: 'blob' }
-        )
-        .subscribe(image => {
-          console.log('image:', image);
-          this.createImageFromBlob(image);
-          console.log('image:', this.image);
-          this.classifier.predict(this.image, (err, results) => {
-            this.result = results[0].className;
-            this.probability = results[0].probability.toFixed(4);
-            console.log(results);
-          });
-    });
   }
 }
