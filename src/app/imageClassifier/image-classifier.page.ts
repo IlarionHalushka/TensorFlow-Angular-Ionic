1bdb573ca66ca8376741a08836133dda81af31a5
IlarionHalushka <hilarion.galushka@gmail.com>
2019-06-16 11:23:35 +0300

    refactor: improve look of img class; remove useless code; fix tslint err

--- src/app/imageClassifier/image-classifier.page.ts
+++ src/app/imageClassifier/image-classifier.page.ts
@@ -1,11 +1,6 @@
 import {Component, Input, OnInit} from '@angular/core';
-import {HttpClient} from '@angular/common/http';
-import { FileUploader } from 'ng2-file-upload';
 
 declare let ml5: any;
-declare let require: any;
-
-const URL = '.';
 
 @Component({
   selector: 'app-image-classifier',
@@ -13,96 +8,38 @@ const URL = '.';
   styleUrls: ['image-classifier.page.scss']
 })
 export class ImageClassifierPage implements OnInit {
-  result: any;
-  imageSrc: string | ArrayBuffer;
-  src = '../../assets/images/plane.jpg';
-  probability: any;
-  results = [{
-    className : null,
-    probability: null,
-  }];
-  inferenceTime = null;
+  MODEL = 'MobileNet';
   IMAGE_SIZE = 224;
+  imageSrc: string | ArrayBuffer;
   predictions: Array<object>;
 
   @Input() classifier;
   @Input() image;
 
-  // public uploader: FileUploader = new FileUploader({ url: URL });
-
-  constructor(private http: HttpClient) {
-    // this.setImageSrc();
+  constructor() {
   }
-
-  // async setImageSrc() {
-  //   const reader = new FileReader();
-  //
-  //   reader.onload = (event) => { // called once readAsDataURL is completed
-  //     this.src = event.target.result;
-  //   };
-  //
-  //   reader.readAsDataURL(this.image.rawFile); // read file as data url
-  // }
 
   ngOnInit() {
-    this.classifier = ml5.imageClassifier('MobileNet', () => {
+    this.classifier = ml5.imageClassifier(this.MODEL, () => {
       console.log('Model Loaded!');
     });
-    // this.setImageSrc();
   }
 
-  // async classifyImage(image) {
-  //   image.width = this.IMAGE_SIZE;
-  //   image.height = this.IMAGE_SIZE;
-  //
-  //   const startTime = performance.now();
-  //   this.results = await this.classifier.classifyImage(image);
-  //   this.inferenceTime = Math.floor(performance.now() - startTime);
-  //   console.log(this.results);
-  // }
-
-  // createImageFromBlob(image: Blob) {
-  //   const reader = new FileReader();
-  //   reader.addEventListener('load', () => {
-  //     this.image = reader.result;
-  //   }, false);
-  //
-  //   if (image) {
-  //     reader.readAsDataURL(image);
-  //   }
-  // }
-
-  predict(img) {
-    // console.log('image:', this.image);
-    // // this.createImageFromBlob(this.image);
-    // console.log('image:', this.image);
-    // const reader = new FileReader();
-    // reader.readAsDataURL(image.rawFile);
-    console.log(img);
-    console.log(img instanceof HTMLImageElement);
-    console.log(img instanceof HTMLCanvasElement);
-    // this.classifier.classify(img, (err, results) => {
-    //   // this.result = results[0].className;
-    //   // this.probability = results[0].probability.toFixed(4);
-    //   console.log(results);
-    // });
-    this.classifier.predict(img, (err, results) => {
-      // this.result = results[0].className;
-      // this.probability = results[0].probability.toFixed(4);
-      this.predictions = results;
-      console.log(results);
-    });
+  async predict(img) {
+    this.predictions = await this.classifier.predict(img);
   }
 
-  readURL(event: Event): void {
-    if (event.target.files && event.target.files[0]) {
-      const file = event.target.files[0];
+  readURL(e: Event): void {
+    const files: FileList = (e.target as HTMLInputElement).files;
+
+    if (files && files[0]) {
+      const file = files[0] as any;
 
       file.width = this.IMAGE_SIZE;
       file.height = this.IMAGE_SIZE;
 
       const reader = new FileReader();
-      reader.onload = e => this.imageSrc = reader.result;
+      reader.onload = () => this.imageSrc = reader.result;
 
       reader.readAsDataURL(file);
     }
